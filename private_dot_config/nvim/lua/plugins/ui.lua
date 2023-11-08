08ac4a23006ee2624f59b9ebc45eed5c0e275378
earlgray <earlgray283@gmail.com>
2023-11-08 10:29:59 +0900

    nvim: disable animation for drawing indentscope

--- private_dot_config/nvim/lua/plugins/ui.lua
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -13,4 +13,13 @@ return {
       },
     },
   },
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      draw = {
+        delay = 0,
+        animation = require("mini.indentscope").gen_animation.none(),
+      },
+    },
+  },
 }
