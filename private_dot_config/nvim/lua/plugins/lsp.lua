83392bde74ad69444bda500bd2b4d8caa68d2903
earlgray <earlgray283@gmail.com>
2023-12-11 12:16:03 +0900

    nvim: use lazyvim linter configs

--- private_dot_config/nvim/lua/plugins/lsp.lua
+++ private_dot_config/nvim/lua/plugins/lsp.lua
@@ -7,32 +7,13 @@ return {
       servers = {
         angularls = {},
 
-        bufls = {},
-
-        clangd = {},
-
         cssls = {},
-
-        dockerls = {},
 
         docker_compose_language_service = {
           root_dir = require("lspconfig/util").root_pattern("docker-compose.yaml", "compose.yaml"),
         },
 
-        gopls = {
-          settings = {
-            gopls = {
-              analyses = {
-                unusedparams = true,
-              },
-              staticcheck = true,
-            },
-          },
-        },
-
         html = {},
-
-        jsonls = {},
 
         lua_ls = {
           -- mason = false, -- set to false if you don't want this server to be installed with mason
@@ -47,46 +28,6 @@ return {
               },
               completion = {
                 callSnippet = "Replace",
-              },
-            },
-          },
-        },
-
-        marksman = {},
-
-        rust_analyzer = {
-          on_attach = function(client)
-            require("completion").on_attach(client)
-          end,
-          settings = {
-            ["rust-analyzer"] = {
-              imports = {
-                granularity = { group = "module" },
-                prefix = "self",
-              },
-              diagnostics = { enable = true },
-              cargo = {
-                buildScripts = { enable = true },
-              },
-              procMacro = {
-                enable = true,
-              },
-              check = {
-                command = "clippy",
-              },
-            },
-          },
-        },
-
-        tailwindcss = {},
-
-        tsserver = {},
-
-        yamlls = {
-          settings = {
-            yaml = {
-              schemas = {
-                ["https://github.com/OAI/OpenAPI-Specification/raw/main/schemas/v3.1/schema.yaml"] = "/*",
               },
             },
           },
