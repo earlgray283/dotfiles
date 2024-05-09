4cbd84a55e56a3626ed5021cac3e521ece594826
earlgray <earlgray@mercari.com>
2024-05-09 23:29:53 +0900

    nvim: editor

--- private_dot_config/nvim/lua/plugins/editor.lua
+++ private_dot_config/nvim/lua/plugins/editor.lua
@@ -7,13 +7,16 @@ return {
 		opts = {
 			diagnostics = {
 				enable = true,
-        show_on_dirs = true,
+				show_on_dirs = true,
 			},
-      modified = {
-        enable = true,
-      },
+			modified = {
+				enable = true,
+			},
 			filters = {
-				dotfiles = true,
+				enable = true,
+				dotfiles = false,
+				git_ignored = false,
+				custom = { "^.git$" },
 			},
 			update_focused_file = {
 				enable = true,
@@ -80,6 +83,8 @@ return {
 		config = true,
 		keys = {
 			{ "<leader>th", "<Cmd>ToggleTerm direction=horizontal<CR>", desc = "Toggle horizontal terminal" },
+			{ "<C-Up>", "<Cmd>resize +2<CR>" },
+			{ "<C-Down>", "<Cmd>resize -2<CR>" },
 		},
 	},
 	{
@@ -166,25 +171,38 @@ return {
 		cmd = { "TroubleToggle", "Trouble" },
 		branch = "dev",
 		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			vim.keymap.set("n", "<leader>xx", function()
-				require("trouble").toggle()
-			end)
-			vim.keymap.set("n", "<leader>xw", function()
-				require("trouble").toggle("workspace_diagnostics")
-			end)
-			vim.keymap.set("n", "<leader>xd", function()
-				require("trouble").toggle("document_diagnostics")
-			end)
-			vim.keymap.set("n", "<leader>xq", function()
-				require("trouble").toggle("quickfix")
-			end)
-			vim.keymap.set("n", "<leader>xl", function()
-				require("trouble").toggle("loclist")
-			end)
-			vim.keymap.set("n", "gR", function()
-				require("trouble").toggle("lsp_references")
-			end)
-		end,
+		keys = {
+			{
+				"<leader>xx",
+				"<cmd>Trouble diagnostics toggle<cr>",
+				desc = "Diagnostics (Trouble)",
+			},
+			{
+				"<leader>xX",
+				"<cmd>Trouble diagnostics toggle filter.buf=0<cr>",
+				desc = "Buffer Diagnostics (Trouble)",
+			},
+			{
+				"<leader>cs",
+				"<cmd>Trouble symbols toggle focus=false<cr>",
+				desc = "Symbols (Trouble)",
+			},
+			{
+				"<leader>cl",
+				"<cmd>Trouble lsp toggle focus=false win.position=right<cr>",
+				desc = "LSP Definitions / references / ... (Trouble)",
+			},
+			{
+				"<leader>xL",
+				"<cmd>Trouble loclist toggle<cr>",
+				desc = "Location List (Trouble)",
+			},
+			{
+				"<leader>xQ",
+				"<cmd>Trouble qflist toggle<cr>",
+				desc = "Quickfix List (Trouble)",
+			},
+		},
+		opts = {}, -- for default options, refer to the configuration section for custom setup.
 	},
 }
