37d9eb01fab3e8ec3635af3da3b378907445b230
earlgray <earlgray@mercari.com>
2024-05-09 23:29:30 +0900

    nvim: add linters for go, proto

--- private_dot_config/nvim/lua/plugins/linting.lua
+++ private_dot_config/nvim/lua/plugins/linting.lua
@@ -3,11 +3,14 @@ return {
 		"mfussenegger/nvim-lint",
 		opts = {
 			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
-			linters_by_ft = {},
+			linters_by_ft = {
+				go = { "golangcilint" },
+				proto = { "buf_lint" },
+			},
 		},
 		config = function(_, opts)
 			require("lint").linters_by_ft = opts.linters_by_ft
-			vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+			vim.api.nvim_create_autocmd(opts.events, {
 				callback = function()
 					require("lint").try_lint()
 				end,
