3b94869fa94870e2fa98d07e6fd127e45a5c5c04
earlgray <earlgray283@gmail.com>
2024-05-12 22:48:24 +0900

    nvim: ui configration(lualine, dressing, nvim-notify)

---
+++ private_dot_config/nvim/lua/plugins/ui.lua
@@ -0,0 +1,50 @@
+return {
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			top_down = false,
+			on_open = function(win)
+				vim.api.nvim_win_set_config(win, { zindex = 100 })
+			end,
+		},
+	},
+	{
+		"stevearc/dressing.nvim",
+		lazy = true,
+		init = function()
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.select = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.select(...)
+			end
+			---@diagnostic disable-next-line: duplicate-set-field
+			vim.ui.input = function(...)
+				require("lazy").load({ plugins = { "dressing.nvim" } })
+				return vim.ui.input(...)
+			end
+		end,
+	},
+	{
+		"nvim-lualine/lualine.nvim",
+		event = "VeryLazy",
+		dependencies = {
+			"nvim-tree/nvim-web-devicons",
+			"gbprod/nord.nvim",
+		},
+		opts = {
+			options = {
+				theme = "auto",
+				globalstatus = true,
+			},
+			sections = {
+				lualine_a = { "mode" },
+				lualine_b = { "branch" },
+				lualine_c = {
+					{ "diagnostics" },
+					{ "filetype", icon_only = true, separator = "", padding = { left = 1, right = 0 } },
+					{ "filename", separator = "", padding = { left = -1 } },
+				},
+			},
+		},
+	},
+}
