a0f124016b2f966ca8d04732ad4d29c0cbc10574
earlgray <earlgray283@gmail.com>
2024-05-12 22:46:40 +0900

    nvim: add nvim-ts-autotag

--- private_dot_config/nvim/lua/plugins/coding.lua
+++ private_dot_config/nvim/lua/plugins/coding.lua
@@ -5,6 +5,11 @@ return {
 		config = true,
 	},
 	{
+		"windwp/nvim-ts-autotag",
+		event = "InsertEnter",
+		opts = {},
+	},
+	{
 		"L3MON4D3/LuaSnip",
 		version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
 		build = "make install_jsregexp",
