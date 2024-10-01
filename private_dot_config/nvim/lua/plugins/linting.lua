451eaf4f23ec04ed0b6c3290d0f004258b27f849
earlgray <earlgray@mercari.com>
2024-10-01 11:10:50 +0900

    nvim: add actionlint

--- private_dot_config/nvim/lua/plugins/linting.lua
+++ private_dot_config/nvim/lua/plugins/linting.lua
@@ -5,20 +5,17 @@ return {
 			events = { "BufWritePost", "BufReadPost", "InsertLeave" },
 			linters_by_ft = {
 				go = { "golangcilint" },
-				proto = { "buf_lint", "api_linter" },
+				proto = { "buf_lint" },
 				typescript = { "eslint" },
 				typescriptreact = { "eslint" },
+        ghaction = {"actionlint"},
 			},
 		},
 		config = function(_, opts)
-			local lint = require("lint")
-			lint.linters.api_linter = {
-				cmd = "api-linter",
-			}
-			lint.linters_by_ft = opts.linters_by_ft
+			require("lint").linters_by_ft = opts.linters_by_ft
 			vim.api.nvim_create_autocmd(opts.events, {
 				callback = function()
-					lint.try_lint()
+					require("lint").try_lint()
 				end,
 			})
 		end,
