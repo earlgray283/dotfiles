c95b1d018995e2b915035e8d0e3ce3543a8c68f9
earlgray <earlgray@mercari.com>
2024-10-01 11:11:17 +0900

    nvim: add commands to togggle conform

--- private_dot_config/nvim/lua/plugins/formatting.lua
+++ private_dot_config/nvim/lua/plugins/formatting.lua
@@ -32,8 +32,27 @@ return {
 			vim.api.nvim_create_autocmd("BufWritePre", {
 				pattern = "*",
 				callback = function(args)
+					if vim.g.disable_autoformat or vim.b[args.buf].disable_autoformat then
+						return
+					end
 					require("conform").format({ bufnr = args.buf })
 				end,
+			})
+			vim.api.nvim_create_user_command("ConformDisable", function(args)
+				if args.bang then
+					-- FormatDisable! will disable formatting just for this buffer
+					vim.b.disable_autoformat = true
+				else
+					vim.g.disable_autoformat = true
+				end
+			end, {
+				desc = "Disable autoformat-on-save",
+			})
+			vim.api.nvim_create_user_command("ConformEnable", function()
+				vim.b.disable_autoformat = false
+				vim.g.disable_autoformat = false
+			end, {
+				desc = "Re-enable autoformat-on-save",
 			})
 		end,
 	},
